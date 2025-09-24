5acc9f09a0499966264e29b4f5f40888d4699e51
Darren Terhune <darrenterhune@gmail.com>
2025-09-24 12:36:11 -0700

    Add provider model and test assertions

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_24_191117) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_24_193058) do
   create_table "clients", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "providers", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
