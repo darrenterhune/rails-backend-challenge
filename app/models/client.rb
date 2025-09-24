f33f9f9f650c74c8166462705773124b3626d00b
Darren Terhune <darrenterhune@gmail.com>
2025-09-24 12:30:41 -0700

    Add client model and test assertions

---
+++ app/models/client.rb
@@ -0,0 +1,4 @@
+class Client < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+end
---
+++ test/models/client_test.rb
@@ -0,0 +1,35 @@
+require "test_helper"
+
+class ClientTest < ActiveSupport::TestCase
+  test "should be valid with valid attributes" do
+    client = clients(:one)
+    assert client.valid?
+  end
+
+  test "should not be valid without a name" do
+    client = clients(:one)
+    client.name = nil
+    assert_not client.valid?
+    assert_includes client.errors[:name], "can't be blank"
+  end
+
+  test "should not be valid without an email" do
+    client = clients(:one)
+    client.email = nil
+    assert_not client.valid?
+    assert_includes client.errors[:email], "can't be blank"
+  end
+
+  test "should not be valid with an invalid email format" do
+    client = clients(:one)
+    client.email = "invalid_email"
+    assert_not client.valid?
+    assert_includes client.errors[:email], "is invalid"
+  end
+
+  test "should be valid with a properly formatted email" do
+    client = clients(:one)
+    client.email = "test@example.com"
+    assert client.valid?
+  end
+end
