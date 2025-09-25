45d2e8cbef6ccb59d6eba2f4edd5cacb9962d96d
Darren Terhune <darrenterhune@gmail.com>
2025-09-24 17:50:18 -0700

    Add appointments controller and tests

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,12 +2,27 @@ class AppointmentsController < ApplicationController
   # POST /appointments
   # Params: client_id, provider_id, starts_at, ends_at
   def create
-    raise NotImplementedError, "Implement appointment booking endpoint"
+    availability = Availability.where(provider_id: appointment_params[:provider_id]).between_dates(params.dig(:appointment, :starts_at), params.dig(:appointment, :ends_at)).first
+    appointment = Appointment.new(appointment_params.merge!(availability_id: availability&.id))
+
+    if appointment.save
+      render(json: appointment, status: :created)
+    else
+      render(json: appointment.errors, status: :unprocessable_entity)
+    end
   end
 
   # DELETE /appointments/:id
   # Bonus: cancel an appointment instead of deleting
   def destroy
-    raise NotImplementedError, "Implement appointment cancelation endpoint"
+    appointment = Appointment.find(params[:id])
+    appointment.soft_delete!
+    render(json: appointment, status: :ok)
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(%i[provider_id client_id])
   end
 end
--- test/controllers/appointments_controller_test.rb
+++ test/controllers/appointments_controller_test.rb
@@ -1,11 +1,30 @@
 require "test_helper"
 
 class AppointmentsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @provider = providers(:one)
+    @client = clients(:one)
+    @availability = availabilities(:three)
+    @starts_at = 5.hours.from_now - 1.minutes
+    @ends_at = 6.hours.from_now + 1.minutes
+  end
+
   test "POST /appointments creates an appointment" do
-    skip "Implement request spec once endpoint is built"
+    post "/appointments", params: { appointment: { client_id: @client.id, provider_id: @provider.id, starts_at: @starts_at, ends_at: @ends_at } }
+    assert_response :success
+    assert_equal "application/json; charset=utf-8", response.content_type
+    appointment = JSON.parse(response.body)
+    assert_equal @client.id, appointment["client_id"]
+    assert_equal @provider.id, appointment["provider_id"]
+    assert_equal @availability.id, appointment["availability_id"]
+    assert_nil appointment["deleted_at"]
   end
 
   test "DELETE /appointments/:id cancels an appointment" do
-    skip "Implement request spec once endpoint is built"
+    appointment = appointments(:two)
+    delete "/appointments/#{appointment.id}"
+    assert_response :success
+    assert_equal "application/json; charset=utf-8", response.content_type
+    assert_not_nil JSON.parse(response.body)["deleted_at"]
   end
 end
